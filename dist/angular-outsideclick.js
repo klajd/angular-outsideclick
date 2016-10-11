c6b6c04768f6cdb5d7404e415172fd8002b86d8b
Klajd <klajd.diko@gmail.com>
2016-10-11 16:27:20 +0200

    build

---
+++ dist/angular-outsideclick.js
@@ -0,0 +1,48 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('ng.outsideclick',[
+            // no dependencies
+        ]);
+
+})();
+(function () {
+    'use strict';
+
+    angular
+        .module('ng.outsideclick')
+        .directive('oursideclick', OutsideclickDirective);
+
+    OutsideclickDirective.$inject = ['$window', '$parse'];
+    function OutsideclickDirective($window, $parse) {
+        return {
+            restrict: 'A',
+            link: link
+        };
+
+        function link(scope, element, attrs) {
+            if (!attrs.oursideclick)
+                return;
+
+            var htmlElement = element[0];
+            var fn = $parse(attrs.oursideclick);
+
+            $window.addEventListener('click', onWindowClick, true);
+
+            scope.$on('$destroy', function (event) {
+                $window.removeEventListener('click', onWindowClick);
+            });
+
+            function onWindowClick(event) {
+                if (htmlElement === event.target || htmlElement.contains(event.target)) {
+                    return;
+                }
+
+                scope.$apply(fn);
+            }
+
+        }
+    }
+
+})();
